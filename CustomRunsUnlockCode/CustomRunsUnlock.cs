70eeb64fd35255585f4a718d75ffb8ba451fedb1
ResistingFate <49793855+ResistingFate@users.noreply.github.com>
2026-03-27 02:13:08 +0000

    Edited the AreAchievementsAndEpochsLocked in GameModeExtension to return false.

---
+++ CustomRunsUnlockCode/CustomRunsUnlock.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Runs;
+
+namespace CustomRunsUnlock.CustomRunsUnlockCode;
+
+[HarmonyPatch(typeof(GameModeExtension), "AreAchievementsAndEpochsLocked")]
+public static class CustomRunsUnlock
+{
+    public static bool Prefix( GameMode gameMode, ref bool __result)
+    {
+        __result = false;
+        return false;
+    }
+}
